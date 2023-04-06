3a54ed459ac80ce94c0c3897abe83daece9bab1c
HussainShipuBD <hussainshipu0356@gmail.com>
2023-04-06 15:23:40 +0600

    first upload

---
+++ src/App.vue
@@ -0,0 +1,146 @@
+ <template>
+  <div id="app">
+    <div id="main-wrapper">
+      <!-- Menu -->
+      <div :class=" scrollY>98 ? '_1menu _1menu_fixed': '_1menu'">
+        <div class="container-fluid">
+          <div class="row align-items-center">
+            <div class="col-12 col-md col-lg">
+              <div class="_1menu_resume">
+                <a href="/static/img/myCv.pdf" download="download">
+                  <button class="_2btn _btn_large">
+                    <span class="_pre_icon"><Icon type="md-document" /></span>  
+                    Download My Resume
+                  </button> 
+                </a>
+              </div>
+            </div>
+
+            <div class="col-12 col-md-auto col-lg-auto">
+              <ul class="_1menu_list">
+                <li>
+                  <a href="#one" @click="tab = 1" :class="tab == 1? '_1menu_list_text _active' : '_1menu_list_text'">About</a>
+                </li>
+                <li>
+                  <a href="#two" @click="tab = 2" :class="tab == 2? '_1menu_list_text _active' : '_1menu_list_text'">Services</a>
+                </li>
+                <li>
+                  <a href="#three" @click="tab = 3" :class="tab == 3? '_1menu_list_text _active' : '_1menu_list_text'">Projects</a>
+                </li>
+                <li>
+                  <a href="#four" @click="tab = 4" :class="tab == 4? '_1menu_list_text _active' : '_1menu_list_text'">Contacts</a>
+                </li>
+              </ul>
+            </div>
+
+            <div class="col-12 col-md col-lg">
+              <div class="_1menu_right">
+                <button class="_1btn _btn_large">
+                  Available For New Projects
+                  <span class="_suf"><Icon type="ios-return-right" /></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- Menu -->
+
+      
+      <!-- Mobile menu -->
+      <div :class=" scrollY>98 ? '_mob_menu _1menu_fixed': '_mob_menu'">
+        <div class="_mob_menu_logo">
+          <h2>Shipu</h2>
+          <!-- <router-link to=""><img class="_mob_menu_logo_img" src="../static/img/logo.png" alt="logo" title="logo"></router-link> -->
+        </div>
+
+        <div class="_mob_menu_button">
+          <Icon @click="isOpen = true" class="_mob_menu_button_icon" type="md-list-box" />
+        </div>
+      </div>
+
+      <div :class="isOpen == true? '_mob_menu_nav_open _mob_menu_nav' : '_mob_menu_nav'">
+        <ul class="_1menu_list">
+          <li>
+            <a @click="isOpen = false" href="#one" class="_1menu_list_text _active">About</a>
+          </li>
+          <li>
+            <a @click="isOpen = false" href="#two" class="_1menu_list_text">Services</a>
+          </li>
+          <li>
+            <a @click="isOpen = false" href="#three" class="_1menu_list_text">Projects</a>
+          </li>
+          <li>
+            <a @click="isOpen = false" href="#four" class="_1menu_list_text">Contacts</a>
+          </li>
+        </ul>
+
+        <p @click="isOpen = false" class="_mob_menu_nav_close"><Icon type="md-close" /></p>
+      </div>
+      <!-- Mobile menu -->
+
+      <div class="_main_layout">
+        <router-view></router-view>
+      </div>
+
+      <!-- Footer -->
+      <div class="container">
+        <div class="_footer">
+          <p class="_footer_text">© year 2022. Designed by <span>Minhajul_abeeden</span>. All rights reserved.</p>
+        </div>
+      </div>
+      <!-- Footer -->
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      isOpen:false,
+      scrollY: 0,
+      tab: 1,
+      bottomOfWindow: 0
+    }
+  },
+
+  methods:{
+    // onClickss(){
+    //   alert('hi');
+    // },
+
+    handleScroll(){
+      this.scrollY = window.scrollY
+    }
+  },
+  
+  created() {
+   
+  },
+
+  mounted(){
+    window.addEventListener('scroll', this.handleScroll);
+
+    window.onscroll = () => {
+      this.bottomOfWindow =  (window.pageYOffset  + window.innerHeight  ) > (document.body.scrollHeight -100);
+      console.log('ssss', this.bottomOfWindow)
+      console.log('inner', window.innerHeight)
+      console.log('page', window.pageYOffset)
+      if (this.bottomOfWindow) {
+        // if (this.noMoreFeed || this.isFeedLoading || this.loadMoreFeed || this.$route.name == 'post-id') {
+        //   return;
+        // }
+        
+        // if(this.$route.name == 'profile-user-tab' || this.$route.name == 'search-tab'  || this.$route.name == 'group-group-tab' || this.$route.name == 'page-user-tab' || this.$route.name == 'events-event-tab'){
+        //    this.fetchMoreProfileFeed();
+        // }else{
+        //    this.fetchNewFeed();
+        // }
+      }
+    };
+
+    console.log('ss', this.scrollY)
+  },
+}
+</script>
---
+++ src/components/index.vue
@@ -0,0 +1,197 @@
+<template>
+    <div>
+        <div class="_1content">
+            <!-- banner -->
+            <div id="one" class="_1banner">
+                <div class="container">
+                    <div class="_1banner_con">
+                        <h1 class="_1banner_title">
+                            Jaxs<span></span>n Westervelt
+                        </h1>
+
+                        <p class="_1banner_text">VIDEO PRODUCTION | DESIGN | ANIMATION | UX DESIGN</p>
+                    </div>
+                </div>
+
+                <div class="_1banner_border">
+                    <div class="_1banner_border_item"></div>
+                    <div class="_1banner_border_item"></div>
+                    <div class="_1banner_border_item"></div>
+                    <div class="_1banner_border_item"></div>
+                </div>
+
+                <div class="_1banner_profile_pic">
+                    <img class="_1banner_profile_img" src="/static/img/profile.png" alt="" title="">
+                </div>
+
+                <ul class="_1banner_social">
+                    <li>
+                        <a href="" class="_1banner_social_icon">
+                            <svg width="11" height="22" viewBox="0 0 11 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M7.51945 21.4439V11.1549H10.4716L10.8629 7.53371H7.51945L7.52441 5.72111C7.52441 4.77661 7.61573 4.27077 8.99646 4.27077H10.8422V0.648926H7.8894C4.34253 0.648926 3.09431 2.40552 3.09431 5.36001V7.53398H0.883301V11.1555H3.09431V21.298C3.95567 21.4666 4.84617 21.5557 5.75794 21.5557C6.34695 21.5557 6.93534 21.5184 7.51945 21.4439Z" fill="white"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="_1banner_social_icon">
+                            <svg width="21" height="21" viewBox="0 0 21 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M4.8129 20.2302V7.22651H0.473318V20.2302H4.81335H4.8129ZM2.64401 5.45139C4.157 5.45139 5.09893 4.45286 5.09893 3.20498C5.07061 1.92867 4.157 0.958008 2.67279 0.958008C1.18756 0.958008 0.217529 1.92867 0.217529 3.20487C0.217529 4.45275 1.15912 5.45127 2.61558 5.45127H2.64367L2.64401 5.45139ZM7.21491 20.2302H11.5541V12.9691C11.5541 12.581 11.5825 12.1919 11.6971 11.9146C12.0107 11.1378 12.7247 10.3337 13.9238 10.3337C15.4936 10.3337 16.122 11.5261 16.122 13.2743V20.2302H20.4611V12.7743C20.4611 8.78027 18.3206 6.92165 15.4655 6.92165C13.1248 6.92165 12.0968 8.2247 11.5256 9.11221H11.5545V7.22696H7.21514C7.27178 8.44686 7.2148 20.2306 7.2148 20.2306L7.21491 20.2302Z" fill="white"/>
+                            </svg>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="" class="_1banner_social_icon">
+                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0 12C0 9.82399 0.536016 7.81601 1.608 5.976C2.68001 4.13599 4.13602 2.68001 5.976 1.608C7.81601 0.535992 9.82402 0 12 0C14.176 0 16.184 0.535992 18.024 1.608C19.864 2.68001 21.32 4.13599 22.392 5.976C23.464 7.81601 24 9.82399 24 12C24 14.176 23.464 16.184 22.392 18.024C21.32 19.864 19.864 21.32 18.024 22.392C16.184 23.464 14.176 24 12 24C9.82402 24 7.81601 23.464 5.976 22.392C4.13602 21.32 2.68001 19.864 1.608 18.024C0.536016 16.184 0 14.176 0 12ZM1.992 12C1.992 14.496 2.832 16.696 4.512 18.6C5.28 17.096 6.49601 15.664 8.16 14.304C9.82402 12.944 11.448 12.088 13.032 11.736C12.792 11.176 12.56 10.672 12.336 10.224C9.58402 11.104 6.60802 11.544 3.408 11.544C2.784 11.544 2.32001 11.536 2.016 11.52C2.016 11.584 2.01202 11.664 2.004 11.76C1.99601 11.856 1.992 11.936 1.992 12ZM2.304 9.528C2.65601 9.55999 3.17602 9.576 3.864 9.576C6.53602 9.576 9.072 9.216 11.472 8.496C10.256 6.336 8.92001 4.536 7.464 3.096C6.20002 3.73601 5.11601 4.62401 4.212 5.76C3.30802 6.89599 2.67202 8.15201 2.304 9.528ZM5.88 19.896C7.68802 21.304 9.72802 22.008 12 22.008C13.184 22.008 14.36 21.784 15.528 21.336C15.208 18.6 14.584 15.952 13.656 13.392C12.184 13.712 10.7 14.52 9.204 15.816C7.70801 17.112 6.6 18.472 5.88 19.896ZM9.552 2.328C10.96 3.78401 12.264 5.59999 13.464 7.776C15.64 6.864 17.28 5.70401 18.384 4.296C16.528 2.76 14.4 1.992 12 1.992C11.184 1.992 10.368 2.10401 9.552 2.328ZM14.328 9.48C14.568 9.99199 14.84 10.64 15.144 11.424C16.328 11.312 17.616 11.256 19.008 11.256C20 11.256 20.984 11.28 21.96 11.328C21.832 9.15199 21.048 7.21601 19.608 5.52C18.568 7.07201 16.808 8.39201 14.328 9.48ZM15.744 13.104C16.56 15.472 17.112 17.904 17.4 20.4C18.664 19.584 19.696 18.536 20.496 17.256C21.296 15.976 21.776 14.592 21.936 13.104C20.768 13.024 19.704 12.984 18.744 12.984C17.864 12.984 16.864 13.024 15.744 13.104Z" fill="white"/>
+                            </svg>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- banner -->
+
+            <!-- description -->
+            <div class="_home_des">
+                <h1 class="_home_2title">Descriptions</h1>
+
+                <div class="container">
+                    <p class="_home_des_text">I am an enthusiastic UX/UI Designer with problem-solving  skills and having 2 years of experience in the user interface 
+                    (UI), wireframing, prototyping, and making the better user 
+                    experiences. I have good knowledge of designing tools 
+                    and  software.</p>
+                </div>
+            </div>
+            <!-- description -->
+
+            <!-- services -->
+            <div id="two" class="_home_ser">
+                <h1 class="_home_title">Services</h1>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <h2 class="_home_ser_subtitle">Services & Capabilities:</h2>
+
+                            <p class="_home_ser_type">Saas Product Design </p>
+                            <p class="_home_ser_type">Website  Design</p>
+                            <p class="_home_ser_type">App Design</p>
+                            <p class="_home_ser_type">User Experience</p>
+                        </div>
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <h2 class="_home_ser_subtitle">Experience</h2>
+
+                            <p class="_home_ser_type">Airtect Ltd <span>(2022-Present)</span></p>
+                            <p class="_home_ser_type">Entra Theme<span>(2019-2022)</span></p>
+                            <p class="_home_ser_type">Applab.com <span>(2016-2019)</span></p>
+                            <p class="_home_ser_type">Expocss <span>(2014-2016)</span></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- services -->
+
+            <!-- projects -->
+            <div id="three" class="_home_proj">
+                <h1 class="_home_title">Projects</h1>
+                
+                <div class="container">
+                    <div class="row g-5">
+                        <!-- Card -->
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <div class="_1card">
+                                <div class="_1card_pic">
+                                    <img class="_1card_img" src="/static/img/project_one.png" alt="" title="">
+                                </div>
+                                <div class="_1card_details">
+                                    <h1 class="_1card_title">Ux Research & Ui Design For NFT Marketplace</h1>
+                                    <p class="_1card_web">Website/Links: <a href="" class="">https://denjonft.com</a></p>
+                                    <p class="_1card_text">Sena makronar opolig fassade. Poledes vafeda öde traress dogt. Gos son polyning rektig dekar. </p>
+
+                                    <button class="_1btn">Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Card -->
+
+                        <!-- Card -->
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <div class="_1card">
+                                <div class="_1card_pic">
+                                    <img class="_1card_img" src="/static/img/project_one.png" alt="" title="">
+                                </div>
+                                <div class="_1card_details">
+                                    <h1 class="_1card_title">Ux Research & Ui Design For NFT Marketplace</h1>
+                                    <p class="_1card_web">Website/Links: <a href="" class="">https://denjonft.com</a></p>
+                                    <p class="_1card_text">Sena makronar opolig fassade. Poledes vafeda öde traress dogt. Gos son polyning rektig dekar. </p>
+
+                                    <button class="_1btn">Read More</button>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- Card -->
+                    </div>
+                </div>
+            </div>
+            <!-- projects -->
+
+            <!-- my introduction -->
+            <div class="_home_intro">
+                <img class="_home_intro_img" src="/static/img/intro.png" alt="" title="">
+            </div>
+            <!-- my introduction -->
+
+            <!-- contact info -->
+            <div id="four" class="_home_contact">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <h2 class="_home_contact_text">LET'S WORK TOGETHER TO MAKE SOMETHING GREAT</h2>
+                        </div>
+
+                        <div class="col-12 col-md-6 col-lg-6">
+                            <div class="_home_contact_info">
+                                <span class="_home_contact_info_icon">
+                                    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <rect x="5.33333" y="8" width="21.3333" height="16" rx="2" stroke="#F4F5F7"/>
+                                        <path d="M5.33333 12L15.1056 16.8861C15.6686 17.1676 16.3314 17.1676 16.8944 16.8861L26.6667 12" stroke="#F4F5F7"/>
+                                    </svg>
+                                </span>
+                                <p class="_home_contact_info_text">hussainshipu0356@gmail.com</p>
+                            </div>
+                            <div class="_home_contact_info">
+                                <span class="_home_contact_info_icon">
+                                    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M13.7071 11.7071L16.3552 14.3552C16.7113 14.7113 16.7113 15.2887 16.3552 15.6448C14.43 17.57 11.3821 17.7866 9.20399 16.153L7.62857 14.9714C5.88504 13.6638 4.33622 12.115 3.02857 10.3714L1.84701 8.79601C0.213407 6.61788 0.430014 3.56999 2.35523 1.64477C2.71133 1.28867 3.28867 1.28867 3.64477 1.64477L6.29289 4.29289C6.68342 4.68342 6.68342 5.31658 6.29289 5.70711L5.27175 6.72825C5.10946 6.89054 5.06923 7.13846 5.17187 7.34373C6.35853 9.71706 8.28294 11.6415 10.6563 12.8281C10.8615 12.9308 11.1095 12.8905 11.2717 12.7283L12.2929 11.7071C12.6834 11.3166 13.3166 11.3166 13.7071 11.7071Z" stroke="#F4F5F7"/>
+                                    </svg>
+                                </span>
+                                <p class="_home_contact_info_text">+880 1790302229</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- contact info -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+
+  data(){
+    return{
+      
+    }
+  },
+
+  methods:{
+    
+  },
+  
+  created(){
+    
+  }
+}
+</script>
